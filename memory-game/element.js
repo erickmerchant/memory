51d61e331dc23235d4a31ea6856fbbc0cd30cdb2
erickmerchant <erickmerchant@gmail.com>
2024-10-15 14:31:34 -0400

    Refactor events

--- memory-game/element.js
+++ memory-game/element.js
@@ -12,7 +12,90 @@ const CHARACTERS = [
 class MemoryGame extends HTMLElement {
 	#incomplete = CHARACTERS.length;
 	#previous;
-	#noMatch;
+	#resolvePrevious;
+	#previousArgs = [];
+
+	#onAnimationEnd = (e) => {
+		if (this.#previousArgs.length) {
+			let [matched, ...elements] = this.#previousArgs.splice(0, Infinity);
+
+			if (!matched) {
+				let {promise, resolve} = Promise.withResolvers();
+
+				this.#resolvePrevious = resolve;
+
+				promise.then(() => {
+					for (let element of elements) {
+						element.className = "covered";
+						element.ariaLabel = "owl";
+					}
+
+					trySong("noMatch");
+				});
+
+				setTimeout(resolve, 2_000);
+			} else {
+				for (let element of elements) {
+					element.className = "matched";
+					element.ariaLabel += " (matched)";
+				}
+
+				this.#incomplete -= 1;
+
+				trySong("match");
+			}
+		} else if (this.#incomplete === 0) {
+			this.#incomplete = -1;
+
+			this.className = "completed";
+
+			scheduleSong("win");
+
+			let reloadDialog = this.querySelector("dialog");
+
+			if (reloadDialog) {
+				reloadDialog.showModal();
+			}
+		}
+	};
+
+	#onClick = (e) => {
+		let current = e.currentTarget;
+
+		if (current.className === "matched") return;
+
+		let isFlipped = current.className === "flipped";
+
+		if (isFlipped) {
+			current.className = "covered";
+			current.ariaLabel = "owl";
+
+			if (this.#previous === current) {
+				this.#previous = null;
+			}
+		} else {
+			current.className = "flipped";
+			current.ariaLabel = current.dataset.name;
+
+			if (this.#previous) {
+				let matched = this.#previous.textContent === current.textContent;
+
+				this.#previousArgs = [matched, this.#previous, current];
+
+				if (!matched) {
+					trySong("reveal");
+				}
+
+				this.#previous = null;
+			} else {
+				trySong("reveal");
+
+				this.#previous = current;
+			}
+		}
+
+		this.#resolvePrevious?.();
+	};
 
 	connectedCallback() {
 		let characters = CHARACTERS.concat(CHARACTERS)
@@ -47,122 +130,30 @@ class MemoryGame extends HTMLElement {
 			faces.append(front, back);
 
 			current.append(faces);
-			current.addEventListener("click", this);
+			current.addEventListener("click", this.#onClick);
 		}
-	}
 
-	handleEvent(e) {
-		let current = e.currentTarget;
+		let reloadDialog = this.querySelector("dialog");
 
-		let faces = current.querySelector(".faces");
-
-		if (current.className === "matched") return;
-
-		let flipped = current.className === "flipped";
-
-		if (flipped) {
-			current.className = "covered";
-			current.ariaLabel = "owl";
-
-			if (this.#previous === current) {
-				this.#previous = null;
-			}
-		} else if (this.#previous) {
-			current.className = "flipped";
-			current.ariaLabel = current.dataset.name;
-
-			let matched = this.#previous.textContent === current.textContent;
-			let previous = this.#previous;
-
-			if (!matched) {
-				trySong("reveal");
-			}
-
-			faces.addEventListener(
-				"animationend",
+		if (reloadDialog) {
+			reloadDialog.querySelector(".play-again")?.addEventListener?.(
+				"click",
 				() => {
-					if (!matched) {
-						let {promise, resolve} = Promise.withResolvers();
-
-						this.#noMatch = resolve;
-
-						promise.then(() => {
-							current.className = "covered";
-							current.ariaLabel = "owl";
-
-							previous.className = "covered";
-							previous.ariaLabel = "owl";
-
-							trySong("noMatch");
-						});
-
-						setTimeout(resolve, 2_000);
-					} else {
-						current.className = "matched";
-						current.ariaLabel += " (matched)";
-
-						previous.className = "matched";
-						previous.ariaLabel += " (matched)";
-
-						this.#incomplete -= 1;
-
-						trySong("match");
-
-						faces.addEventListener(
-							"animationend",
-							() => {
-								if (this.#incomplete) return;
-
-								this.className = "completed";
-
-								scheduleSong("win");
-
-								let reloadDialog = this.querySelector("dialog");
-
-								if (reloadDialog) {
-									reloadDialog.showModal();
-
-									reloadDialog.querySelector("#playAgain")?.addEventListener?.(
-										"click",
-										() => {
-											window.location.reload();
-										},
-										{once: true}
-									);
-
-									reloadDialog
-										.querySelector("#stopPlaying")
-										?.addEventListener?.(
-											"click",
-											() => {
-												reloadDialog.close();
-											},
-											{once: true}
-										);
-								}
-							},
-							{
-								once: true,
-							}
-						);
-					}
+					window.location.reload();
 				},
-				{
-					once: true,
-				}
+				{once: true}
 			);
 
-			this.#previous = null;
-		} else {
-			current.className = "flipped";
-			current.ariaLabel = current.dataset.name;
-
-			trySong("reveal");
-
-			this.#previous = current;
+			reloadDialog.querySelector(".stop-playing")?.addEventListener?.(
+				"click",
+				() => {
+					reloadDialog.close();
+				},
+				{once: true}
+			);
 		}
 
-		this.#noMatch?.();
+		this.addEventListener("animationend", this.#onAnimationEnd);
 	}
 }
 
