0829690bbe4d452d35d0506bd85c2f2900ab06f5
erickmerchant <erickmerchant@gmail.com>
2025-11-01 23:45:51 -0400

    Move files out of src

--- flint.ts
+++ flint.ts
@@ -1,10 +1,10 @@
 import css from "@flint/framework/handlers/css";
 import js from "@flint/framework/handlers/js";
 import flint from "@flint/framework";
-import index from "./src/pages/index.ts";
-import halloween from "./src/pages/halloween.ts";
+import index from "./pages/index.ts";
+import halloween from "./pages/halloween.ts";
 
-const app = flint("src", "dist")
+const app = flint()
   .route("/", index)
   .file("/index.js", js)
   .file("/index.css", css)
