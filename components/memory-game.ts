55284c9b21ccc10e5fbde17a9c3f860b908e48c1
erickmerchant <erickmerchant@gmail.com>
2026-02-04 00:05:54 -0500

    Upgrade handcraft

--- components/memory-game.ts
+++ components/memory-game.ts
@@ -1,5 +1,5 @@
 import type { Song } from "./audio.ts";
-import { $, define, h, observe, watch, when } from "@handcraft/lib";
+import { $, define, h, watch, when } from "@handcraft/lib";
 import { scheduleSong, trySong } from "./audio.ts";
 
 export type Character = { text: string; name: string; color: string };
@@ -27,177 +27,164 @@ type State = {
 const { span, div, dialog, p, button } = h.html;
 
 export const memoryGame = (settings: Settings) => {
-  const tag = define("memory-game").setup((host) => {
-    const state: State = watch({
-      incomplete: settings.characters.length,
-      modalOpen: false,
-      characters: watch<Array<CharacterAndState>>([]),
-      previous: null,
-    });
-    const observed = observe(host);
+  const tag = define("memory-game", {
+    connected(host) {
+      const state: State = watch({
+        incomplete: settings.characters.length,
+        modalOpen: false,
+        characters: watch<Array<CharacterAndState>>([]),
+        previous: null,
+      });
 
-    resetState();
+      resetState();
 
-    const reloadDialog = () =>
-      dialog.class("reload-dialog").effect(
-        (el: HTMLDialogElement) => {
-          if (state.modalOpen) {
-            el.showModal();
-          } else {
-            el.close();
-          }
-        },
-      )(
-        div.class("card")(
-          div.class("faces")(span.class("front face")("🦉")),
-        ),
-        div.class("bubble")(
-          p("Hoo-ray! You found all my owl friends."),
-          button.class("play-again").on("click", resetState)("Play Again!"),
-        ),
-      );
-
-    $(host)(
-      observed("> button").map(
-        (button, i) => {
-          const character = state.characters[i];
-
-          const faces = div.class("faces").style({
-            "--turns": () => character.total,
-            "--duration": () => character.latest,
-            "--background": () => `var(--${character.color})`,
-          })(
-            span.class("front face")("🦉"),
-            span.class("back face")(span.class("text")(() => character.text)),
-          );
-          const clickCard = (e: Event) => {
-            if (e.currentTarget == null) return;
-
-            if (!character.interactive) {
-              return;
+      const reloadDialog = () =>
+        dialog.class("reload-dialog").effect(
+          (el: HTMLDialogElement) => {
+            if (state.modalOpen) {
+              el.showModal();
+            } else {
+              el.close();
             }
+          },
+        )(
+          div.class("card")(
+            div.class("faces")(span.class("front face")("🦉")),
+          ),
+          div.class("bubble")(
+            p("Hoo-ray! You found all my owl friends."),
+            button.class("play-again").on("click", resetState)("Play Again!"),
+          ),
+        );
 
-            if (!character.revealed) {
-              if (state.previous) {
-                const previous = state.previous;
+      $(host)(
+        state.characters.map(
+          (character) => {
+            const faces = div.class("faces").style({
+              "--turns": () => character.total,
+              "--duration": () => character.latest,
+              "--background": () => `var(--${character.color})`,
+            })(
+              span.class("front face")("🦉"),
+              span.class("back face")(span.class("text")(() => character.text)),
+            );
+            const clickCard = (e: Event) => {
+              if (e.currentTarget == null) return;
 
-                turn(character, 1);
+              if (!character.interactive) {
+                return;
+              }
 
-                trySong(settings.songs.reveal);
+              if (!character.revealed) {
+                if (state.previous) {
+                  const previous = state.previous;
 
-                character.interactive = false;
-                previous.interactive = false;
+                  turn(character, 1);
 
-                const matching = character.text === state.previous.text;
+                  trySong(settings.songs.reveal);
 
-                $(e.currentTarget as Element).on("transitionend", () => {
-                  if (matching) {
-                    turn(character, 2);
-                    turn(previous, 2);
+                  character.interactive = false;
+                  previous.interactive = false;
 
-                    state.incomplete -= 1;
+                  const matching = character.text === state.previous.text;
 
-                    if (state.incomplete === 0) {
-                      scheduleSong(settings.songs.win);
+                  $(e.currentTarget as Element).on("transitionend", () => {
+                    if (matching) {
+                      turn(character, 2);
+                      turn(previous, 2);
 
-                      for (const character of state.characters) {
-                        turn(character, 6);
+                      state.incomplete -= 1;
+
+                      if (state.incomplete === 0) {
+                        scheduleSong(settings.songs.win);
+
+                        for (const character of state.characters) {
+                          turn(character, 6);
+                        }
+
+                        state.modalOpen = true;
+
+                        state.incomplete = -1;
+                      } else {
+                        scheduleSong(settings.songs.match);
                       }
-
-                      state.modalOpen = true;
-
-                      state.incomplete = -1;
                     } else {
-                      scheduleSong(settings.songs.match);
+                      setTimeout(() => {
+                        turn(character, 1);
+                        turn(previous, 1);
+
+                        character.interactive = true;
+                        previous.interactive = true;
+
+                        trySong(settings.songs.cover);
+                      }, 1_000);
                     }
-                  } else {
-                    setTimeout(() => {
-                      turn(character, 1);
-                      turn(previous, 1);
+                  }, { once: true });
 
-                      character.interactive = true;
-                      previous.interactive = true;
+                  state.previous = null;
+                } else {
+                  turn(character, 1);
 
-                      trySong(settings.songs.cover);
-                    }, 1_000);
-                  }
-                }, { once: true });
+                  trySong(settings.songs.reveal);
 
-                state.previous = null;
+                  state.previous = character;
+                }
               } else {
                 turn(character, 1);
 
-                trySong(settings.songs.reveal);
+                state.previous = null;
 
-                state.previous = character;
+                trySong(settings.songs.cover);
               }
-            } else {
-              turn(character, 1);
+            };
 
-              state.previous = null;
+            return button
+              .aria({
+                label:
+                  () => (character.total % 2 === 0 ? "owl" : character.name),
+              })
+              .on("click", clickCard)(faces);
+          },
+        ),
+        when((prev) => prev || state.modalOpen).show(reloadDialog),
+      );
 
-              trySong(settings.songs.cover);
-            }
-          };
+      function resetState() {
+        state.incomplete = settings.characters.length;
+        state.modalOpen = false;
 
-          return button
-            .aria({
-              label: () => (character.total % 2 === 0 ? "owl" : character.name),
-            })
-            .on("click", clickCard)(faces);
-        },
-      ),
-      when((prev) => prev || state.modalOpen).show(reloadDialog),
-    );
+        const characters = settings.characters
+          .concat(settings.characters)
+          .map((character) => {
+            return {
+              ...character,
+              interactive: true,
+              order: Math.random(),
+              total: 0,
+              latest: 0,
+              revealed: false,
+            };
+          })
+          .toSorted((a, b) => a.order - b.order);
 
-    function resetState() {
-      state.incomplete = settings.characters.length;
-      state.modalOpen = false;
-
-      const characters = settings.characters
-        .concat(settings.characters)
-        .map((character) => {
-          return {
-            ...character,
-            interactive: true,
-            order: Math.random(),
-            total: 0,
-            latest: 0,
-            revealed: false,
-          };
-        })
-        .toSorted((a, b) => a.order - b.order);
-
-      for (let i = 0; i < characters.length; i++) {
-        if (state.characters[i]) {
-          Object.assign(state.characters[i], characters[i]);
-        } else {
-          state.characters.push(watch(characters[i]));
+        for (let i = 0; i < characters.length; i++) {
+          if (state.characters[i]) {
+            Object.assign(state.characters[i], characters[i]);
+          } else {
+            state.characters.push(watch(characters[i]));
+          }
         }
       }
-    }
 
-    function turn(model: CharacterAndState, val: number) {
-      model.total += val;
+      function turn(model: CharacterAndState, val: number) {
+        model.total += val;
 
-      model.latest = val ?? 0;
+        model.latest = val ?? 0;
 
-      model.revealed = (model.total % 2) === 1;
-    }
+        model.revealed = (model.total % 2) === 1;
+      }
+    },
   });
 
-  return tag(
-    button,
-    button,
-    button,
-    button,
-    button,
-    button,
-    button,
-    button,
-    button,
-    button,
-    button,
-    button,
-  );
+  return tag;
 };
