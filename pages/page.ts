0567800c298a0631d1bc11d95e131e984af96e73
erickmerchant <erickmerchant@gmail.com>
2026-09-02 13:35:19 -0700

    Update

--- pages/page.ts
+++ pages/page.ts
@@ -21,7 +21,7 @@ export default function (
       meta
         .name("viewport")
         .content(
-          "width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0",
+          "width=device-width, maximum-scale=1.0, user-scalable=0",
         ),
       title("Memory"),
       scriptOrStyles,
